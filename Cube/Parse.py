157a2ea9522d131f9b2fbb40223f21fb0c89777f
Lucas John Jacobson <lucasj@tux-115.cae.wisc.edu>
2014-03-31 13:55:27 -0500

    added support for custom length tolerance

--- Cube/Parse.py
+++ Cube/Parse.py
@@ -2,13 +2,15 @@ import itertools
 import os
 
 # Determine filename
-def determine_filename(version,geom,N,F,mfp):
+def determine_filename(version,geom,N,F,mfp,tol):
     
     N_str   = ('%5u'   % (N  )).replace(' ','_')
     F_str   = ('%2.0f' % (F  )).replace(' ','_')
     mfp_str = ('%6.2f' % (mfp)).replace(' ','_')
+    tol_str = ('%3.0f' % (tol)).replace(' ','_')
     
-    fname = 'zCube_%s_%s_%s_%s_%s' % (version,geom,N_str,F_str,mfp_str)          # base filename (no extension)
+    fname = 'zCube_%s_%s_%s_%s_%s_%s' % \
+            (version,geom,N_str,F_str,mfp_str,tol_str)
     return fname
 
 # Parse output files for CTM and NPS
@@ -18,7 +20,7 @@ def parse_output_file(fname,version,geom,N,F,mfp):
     fname_out = '%s.out' % (fname)
     
     # Make sure output file is valid
-    if (os.path.isfile(direc+fname_io) == False) or (os.path.getsize(direc+fname_io) == 0) or (os.path.getsize(direc+fname_out) == 0):
+    if (os.path.isfile(direc+fname_io) == False):
         nps = -1
         ctm1 = -1
         ctm2 = -1
@@ -83,6 +85,8 @@ F_vals   = [float(i) for i in params_str[5].split()[1:]]                        
 mfps     = [float(i) for i in params_str[6].split()[1:]]                        # list of number of mean free paths in 1 m
 ctme     =  float(            params_str[7].split()[1 ])                        # computer time
 mfp_in   =  float(            params_str[8].split()[1 ])                        # mfp for pre-existing LCAD file
+toltype  =                    params_str[9].split()[1 ]  # type of faceting tolerance to use for DAG
+tols     = [float(i) for i in params_str[9].split()[2:]] # list of faceting tolerances to use for DAG
 
 reader.close()
 
@@ -90,15 +94,16 @@ if os.path.isfile(resdir+'Cube_results.txt') == True:
     os.remove(resdir+'Cube_results.txt')
 
 # Parse output
-for params in list(itertools.product(versions,geoms,N_vals,F_vals,mfps)):
+for params in list(itertools.product(versions,geoms,N_vals,F_vals,mfps,tols)):
     
     version = params[0]                                                         # loop for all MCNP versions
     geom    = params[1]                                                         # loop for all geometry configurations
     N       = params[2]                                                         # loop for all values of N
     F       = params[3]                                                         # loop for all values of F
     mfp     = params[4]                                                         # loop for all values of mfp
+    tol     = params[5]                                                         # loop for all values of tol
     
-    fname = determine_filename(version,geom,N,F,mfp)                            # base filename (no extension)
+    fname = determine_filename(version,geom,N,F,mfp,tol)                            # base filename (no extension)
     
     parse_output_file(fname,version,geom,N,F,mfp)                               # parse output file for CTM and NPS
     print fname
